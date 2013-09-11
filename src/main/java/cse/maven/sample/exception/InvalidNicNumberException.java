bed97944d5fd8d464c346e9916e3c0380f947114
Pranavan Theivendiram <234rana@gmail.com>
2013-09-11 22:01:14 +0530

    Revert "This is my first commit"
    This reverts commit 2bec25c2d621ccb8082061877baba17055b54893.

--- src/main/java/cse/maven/sample/exception/InvalidNicNumberException.java
+++
@@ -1,8 +0,0 @@
-package cse.maven.sample.exception;
-
-public class InvalidNicNumberException extends Exception {
-
-	public InvalidNicNumberException(String nic) {
-		super("Invalid NIC number is entered " + nic);
-	}
-}
